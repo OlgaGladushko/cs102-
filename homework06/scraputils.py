d8ad24a29af83ee953edd2a19f620949b249b380
OlgaGladushko <71010896+OlgaGladushko@users.noreply.github.com>
2021-05-29 22:02:26 +0300

    Update scraputils.py
    part 1

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,19 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+from db import News, session
 
 
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
 
-    # PUT YOUR CODE HERE
+    table = (parser.find('table', {'class': 'itemlist'}))
+    trs = table.find_all("tr")
+    count = 0
+    dict_add = {
+        'author': None,
+        'comments': 0,
+        'points': 0,
+        'title': None,
+        'url': None
+    }
+    
+    for i, elem in enumerate(trs):
+        if i < 90:
+            if count == 0:
+                dict_add["title"] = elem.find('a', {'class': 'storylink'}).get_text()
+                dict_add['url'] = (elem.find('a', {'class': 'storylink'}))['href']
+            if count == 1:
+                str_point=(elem.find('span', {'class': 'score'})).get_text()
+                dict_add['points'] = int(str_point[:str_point.find(" ")])
+                dict_add['author'] = elem.find('a', {'class': 'hnuser'}).get_text()
+                a_els = elem.find_all("a")
+                if a_els[-1].get_text() == "discuss":
+                    dict_add['comments'] = 0
+                else:
+                    str_comm = a_els[-1].get_text()
+                    dict_add['comments'] = int(str_comm[:str_comm.find(" ")])
+            if count == 2:
+                news_list.append(dict_add)
+                count = -1
+                dict_add = {
+                    'author': None,
+                    'comments': 0,
+                    'points': 0,
+                    'title': None,
+                    'url': None
+                }
+            count += 1
 
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    table = (parser.find('table', {'class': 'itemlist'}))
+    trs = table.find_all("tr")
+    return trs[-1].find('a', {'class': 'morelink'})['href']
 
 
 def get_news(url, n_pages=1):
@@ -28,5 +67,6 @@ def get_news(url, n_pages=1):
         url = "https://news.ycombinator.com/" + next_page
         news.extend(news_list)
         n_pages -= 1
+        print(len(news))
+        
     return news
-
